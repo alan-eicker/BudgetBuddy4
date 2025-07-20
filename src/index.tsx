06cc046c54c9d68d0c9c8cf17d0be409f44b135a
Alan Eicker <alaneicker@gmail.com>
2025-07-20 09:14:48 -0500

    chore(deps): update React and related dependencies; switch to react-helmet-async

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import AppProvider from './shared/providers/AppProvider';
 import reportWebVitals from './reportWebVitals';
@@ -10,11 +12,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </Router>
+    <HelmetProvider>
+      <BrowserRouter>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
 );
 
