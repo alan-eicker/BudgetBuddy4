6e2d92886abd4b350a17cccc37fc112cd849830b
Alan Eicker <alaneicker@gmail.com>
2025-07-23 13:34:10 -0500

    feat: update webpack and package configurations for improved build process and add environment variable support

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,5 @@
 import { initializeApp } from '@firebase/app';
 import { getFirestore } from '@firebase/firestore';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
