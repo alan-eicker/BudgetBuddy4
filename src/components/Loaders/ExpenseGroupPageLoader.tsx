a1b7710bf87e36ee5d0bf81a827baec54d436b42
Alan Eicker <alaneicker@gmail.com>
2025-07-23 19:05:06 -0500

    feat: enhance ExpenseGroupPageLoader with structured header and button loaders

--- src/components/Loaders/ExpenseGroupPageLoader.tsx
+++ src/components/Loaders/ExpenseGroupPageLoader.tsx
@@ -4,10 +4,23 @@ import styles from './ExpenseGroupPageLoader.module.scss';
 const ExpenseGroupPageLoader = () => {
   return (
     <div>
-      <ContentLoader viewBox="0 0 800 130" height={130} width={800}>
-        <rect x="0" y="0" rx="4" ry="4" width="800" height="30" />
-        <rect x="0" y="45" rx="4" ry="4" width="350" height="20" />
-      </ContentLoader>
+      <div className={styles.header}>
+        <ContentLoader viewBox="0 0 800 60" height={60} width={800}>
+          <rect x="0" y="0" rx="4" ry="4" width="800" height="30" />
+          <rect x="0" y="45" rx="4" ry="4" width="350" height="20" />
+        </ContentLoader>
+        <div className={styles.buttons}>
+          <ContentLoader viewBox="0 0 100 40" height={40} width={100}>
+            <rect x="0" y="0" rx="4" ry="4" width="800" height="40" />
+          </ContentLoader>
+          <ContentLoader viewBox="0 0 100 40" height={40} width={100}>
+            <rect x="0" y="0" rx="4" ry="4" width="800" height="40" />
+          </ContentLoader>
+          <ContentLoader viewBox="0 0 100 40" height={40} width={100}>
+            <rect x="0" y="0" rx="4" ry="4" width="800" height="40" />
+          </ContentLoader>
+        </div>
+      </div>
       <div className={styles.expenseItems}>
         <ContentLoader viewBox="0 0 1024 120" height={120} width={1024}>
           <rect x="0" y="0" rx="4" ry="4" width="1024" height="100" />
