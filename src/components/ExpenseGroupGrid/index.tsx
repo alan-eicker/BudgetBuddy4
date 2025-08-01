ba158248bc043704980e48d5b32c4d439277fe5b
Alan Eicker <alaneicker@gmail.com>
2025-08-01 10:47:47 -0500

    Add logic to display overdue alert for expense group grid

--- src/components/ExpenseGroupGrid/index.tsx
+++ src/components/ExpenseGroupGrid/index.tsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
 import Icon from '../Icon';
 
 import { ExpenseGroup } from '../../shared/types/expenseGroups';
 
 import { toDollarAmountString } from '../../utils/numbers';
+import { isOverDue } from '../../utils/dates';
+
+import useAppProvider from '../../shared/hooks/useAppProvider';
 
 import styles from './ExpenseGroupGrid.module.scss';
 
@@ -13,26 +17,45 @@ interface ExpenseGroupGridProps {
 }
 
 const ExpenseGroupGrid = ({ expenseGroupData = [] }: ExpenseGroupGridProps) => {
+  const { allExpenses } = useAppProvider();
+
   return (
     <div className={styles.expenseGroupGrid}>
-      {expenseGroupData.map((group) => (
-        <Link
-          key={group.id}
-          className={styles.expenseGroupCard}
-          to={`/expense-group/${group.id}`}
-        >
-          <div>
-            <h3 className={styles.groupTitle}>
-              {new Date(group.startDate).toDateString()} -{' '}
-              {new Date(group.endDate).toDateString()}
-            </h3>
-            <div className={styles.groupBudget}>
-              Total Budget: {toDollarAmountString(group.totalBudget)}
+      {expenseGroupData.map((group) => {
+        const overdueExpenses = allExpenses
+          .filter((expense) => expense.expenseGroupId === group.id)
+          .map((expense) => isOverDue(expense.dueDate))
+          .filter(Boolean);
+
+        const hasOverdueExpenses = overdueExpenses.length > 0;
+
+        return (
+          <Link
+            key={group.id}
+            className={classnames(styles.expenseGroupCard, {
+              [styles.hasOverdueExpenses]: hasOverdueExpenses,
+            })}
+            to={`/expense-group/${group.id}`}
+          >
+            <div>
+              {hasOverdueExpenses && (
+                <div className={styles.overDueAlert} role="alert">
+                  {overdueExpenses.length} overdue expenses
+                </div>
+              )}
+              <h3 className={styles.groupTitle}>
+                {new Date(group.startDate).toDateString()} -{' '}
+                {new Date(group.endDate).toDateString()}
+              </h3>
+              <div className={styles.groupBudget}>
+                {}
+                Total Budget: {toDollarAmountString(group.totalBudget)}
+              </div>
             </div>
-          </div>
-          <Icon name="ChevronRight" />
-        </Link>
-      ))}
+            <Icon name="ChevronRight" />
+          </Link>
+        );
+      })}
     </div>
   );
 };
