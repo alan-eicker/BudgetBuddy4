ec9c7471219d960212465da106a45a7d0ab35c79
Alan Eicker <alaneicker@gmail.com>
2025-08-03 15:55:56 -0500

    refactoring

--- src/features/LogoutButton/index.tsx
+++ src/features/LogoutButton/index.tsx
@@ -1,31 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 
-import { signOut } from "firebase/auth";
-import { auth } from "../../firebase";
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebase';
 
 import Button from '../../components/Button';
 
 const LogoutButton = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const logoutUser = async (): Promise<void> => {
-        try {
-            await signOut(auth);
-            navigate('/');
-            console.log("Sign‑out successful.");
-        } catch (error: any) {
-            console.error("Error signing out:", error);
-        }
+  const logoutUser = async (): Promise<void> => {
+    try {
+      await signOut(auth);
+      navigate('/');
+    } catch (error: any) {
+      console.error('Error signing out:', error);
     }
+  };
 
-    return (
-        <Button
-        size="sm"
-        text="Log Out"
-        variant="primary"
-        onClick={logoutUser}
-        />
-    );
+  return (
+    <Button size="sm" text="Log Out" variant="primary" onClick={logoutUser} />
+  );
 };
 
 export default LogoutButton;
