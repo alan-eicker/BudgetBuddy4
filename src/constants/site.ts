3e1194b3c25fd48e9ede0f86b38857e3d8a47e7f
Alan Eicker <alaneicker@gmail.com>
2025-08-03 16:19:29 -0500

    Updates nav

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -2,6 +2,9 @@ export const BASE_URL = '/';
 
 export const APP_NAME = 'Budget Buddy';
 
-export const NAV = [{ text: 'Dashboard', url: '/dashboard' }];
+export const NAV = [
+  { text: 'Dashboard', url: '/dashboard' },
+  { text: 'Add Expense Group', url: '/expense-goup/add' },
+];
 
 export const COPYRIGHT_TEXT = `${new Date().getFullYear()} Budget Buddy. All rights reserved.`;
