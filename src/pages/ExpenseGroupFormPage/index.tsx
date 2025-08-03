62763bfba89e5ac608124d5b645ca4a3dd587ee3
Alan Eicker <alaneicker@gmail.com>
2025-08-03 16:21:25 -0500

    Updates doc title

--- src/pages/ExpenseGroupFormPage/index.tsx
+++ src/pages/ExpenseGroupFormPage/index.tsx
@@ -7,13 +7,13 @@ const ExpenseGroupFormPage = () => {
   const portalTarget = document.querySelector('#hero-section');
 
   const heroContent = portalTarget
-    ? createPortal(<h1>Add Expense Group</h1>, portalTarget)
+    ? createPortal(<h1>Budget Buddy</h1>, portalTarget)
     : null;
 
   return (
     <>
       <Helmet>
-        <title>Add Expense Group</title>
+        <title>Add Expense Group | Add Expense Group</title>
       </Helmet>
       {heroContent}
       <ExpenseGroupForm />
